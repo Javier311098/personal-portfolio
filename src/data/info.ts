cd0854ac71c01ae4c05fe869e50711f8f134e359
Javier Martinez <javiermartinez311098@gmail.com>
2024-06-01 15:50:22 -0600

    editing information and adding a new logo

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,79 +1,70 @@
 export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
   name: "Javier Martinez",
-  jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  jobDescription: "Full Stack JavaScript Developer",
+  about: `Full Stack JavaScript Developer with a strong background in web application development. 
+  Specialized in Angular with over two years of experience, as well as proficient in React. 
+  Passionate about using JavaScript (JS) and TypeScript (TS) for web development. Skilled at problem-solving through code. 
+  Considered a responsible team player adept at collaborative work.
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
+      name: "Improving",
+      location: "Jalisco, México",
+      startDate: "May 2021",
       endDate: "Present",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        "- Work on the frontend using React and Redux for the development of a remote interview tool.",
+        "- Application of agile methodologies such as SCRUM.",
+        "- Development of new components and hooks in React.",
+        "- Learning Angular and start working in component using Angular Material.",
+        "- Use of external libraries tailored to client needs.",
+        "- Use of tools like Git, GitHub, GitLab.",
       ],
     },
 
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: "Universidad Enrique Díaz de León (Project to Obtain Title in Software Engineering)",
+      location: "Jalisco, México",
+      startDate: "Aug 2022",
+      endDate: "Dec 2022",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
+        "- Full development of a system to manage a preschool.",
+        "- Use of React for the frontend and Node.js for the backend.",
+        "- Creation of a database using PostgreSQL.",
+        "- Creation of APIs to manage the database and retrieve data to the frontend for user display.",
+        "- Use of JWT for user verification. ",
       ],
     },
 
     {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
+      name: "Beliveo",
+      location: "Jalisco, México",
+      startDate: "Aug 2020",
+      endDate: "Apr 2021",
       description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "- Development of soft skills such as speaking and listening in English.",
+        "- Problem-solving based on client needs.",
+        "- Problem-solving using active listening.",
       ],
     },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
-      description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
-      ],
-    },
-    {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
+      name: "Universidad Enrique Díaz de León",
+      location: "Jalisco, México",
       startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
+      endDate: "2020",
+      description: ["Software Engineering"],
+    },
+    {
+      name: "Instituto Cultural Mexicano Norteamericano de Jalisco",
+      location: "Jalisco, México",
+      startDate: "2017",
+      endDate: "2019",
+      description: ["ELASH Certificate Acquisition"],
     },
   ],
 
